63ebc30ef13fc7481c035dc4e407a054991de62c
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-07-12 12:08:48 -0700

    Parse RDS containing GenomicRangesList

--- src/rds2py/granges.py
+++ src/rds2py/granges.py
@@ -1,6 +1,6 @@
-from genomicranges import GenomicRanges, SeqInfo
-from iranges import IRanges
 from biocframe import BiocFrame
+from genomicranges import GenomicRanges, GenomicRangesList, SeqInfo
+from iranges import IRanges
 
 from .parser import get_class
 from .pdf import as_pandas_from_dframe
@@ -44,25 +44,17 @@ def as_granges(robj):
             _strands = [_levels_data[x] for x in _strands]
 
     _seqinfo_seqnames = robj["attributes"]["seqinfo"]["attributes"]["seqnames"]["data"]
-    _seqinfo_seqlengths = robj["attributes"]["seqinfo"]["attributes"]["seqlengths"][
-        "data"
-    ]
-    _seqinfo_is_circular = robj["attributes"]["seqinfo"]["attributes"]["is_circular"][
-        "data"
-    ]
+    _seqinfo_seqlengths = robj["attributes"]["seqinfo"]["attributes"]["seqlengths"]["data"]
+    _seqinfo_is_circular = robj["attributes"]["seqinfo"]["attributes"]["is_circular"]["data"]
     _seqinfo_genome = robj["attributes"]["seqinfo"]["attributes"]["genome"]["data"]
     _seqinfo = SeqInfo(
         seqnames=_seqinfo_seqnames,
         seqlengths=[None if x == -2147483648 else int(x) for x in _seqinfo_seqlengths],
-        is_circular=[
-            None if x == -2147483648 else bool(x) for x in _seqinfo_is_circular
-        ],
+        is_circular=[None if x == -2147483648 else bool(x) for x in _seqinfo_is_circular],
         genome=_seqinfo_genome,
     )
 
-    _mcols = BiocFrame.from_pandas(
-        as_pandas_from_dframe(robj["attributes"]["elementMetadata"])
-    )
+    _mcols = BiocFrame.from_pandas(as_pandas_from_dframe(robj["attributes"]["elementMetadata"]))
 
     _gr_names = None
     if "NAMES" in robj["attributes"]:
@@ -111,3 +103,36 @@ def _as_list(robj):
         _data = _final
 
     return _data
+
+
+def as_granges_list(robj):
+    """Parse an R object as a :py:class:`~genomicranges.GenomicRangesList.GenomicRangesList`.
+
+    Args:
+        robj:
+            Object parsed from the `RDS` file.
+
+            Usually the result of :py:func:`~rds2py.parser.read_rds`.
+
+    Returns:
+        A ``GenomicRangesList`` object.
+    """
+
+    _cls = get_class(robj)
+
+    if _cls not in ["CompressedGRangesList", "GRangesList"]:
+        raise TypeError(f"obj is not genomic ranges list, but is `{_cls}`.")
+
+    _gre = as_granges(robj["attributes"]["unlistData"])
+
+    _groups = robj["attributes"]["partitioning"]["attributes"]["NAMES"]["data"]
+    _partitionends = robj["attributes"]["partitioning"]["attributes"]["end"]["data"]
+
+    _grelist = []
+
+    current = 0
+    for _pend in _partitionends:
+        _grelist.append(_gre[current:_pend])
+        current = _pend
+
+    return GenomicRangesList(ranges=_grelist, names=_groups)
--- tests/test_granges.py
+++ tests/test_granges.py
@@ -1,9 +1,9 @@
 import pytest
 
-from rds2py.granges import as_granges
+from rds2py.granges import as_granges, as_granges_list
 from rds2py.parser import read_rds
 
-from genomicranges import GenomicRanges
+from genomicranges import GenomicRanges, GenomicRangesList
 
 __author__ = "jkanche"
 __copyright__ = "jkanche"
@@ -16,3 +16,12 @@ def test_granges():
     gr = as_granges(robj=robj)
 
     assert isinstance(gr, GenomicRanges)
+
+
+def test_granges_list():
+    robj = read_rds("tests/data/grangeslist.rds")
+
+    gr = as_granges_list(robj=robj)
+
+    assert isinstance(gr, GenomicRangesList)
+    assert len(gr) == 5
